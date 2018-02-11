41c2e398d9705daf975e5bb3294290dd674cda50
Alison Lui <Alison@Alisons-MacBook-Pro.local>
2018-02-10 18:03:04 -0600

    create game_state object

---
+++ game_state.js
@@ -0,0 +1,111 @@
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+}
+
+function Player(id, name){
+	this.id = id;
+	this.role = "";
+	this.name = name;
+	this.voteStatus = true;
+	this.vote = function(v){
+		this.voteStatus = v;
+	}
+}
+
+function Deck(){
+	this.drawPile = [
+		"celery",
+		"celery",
+		"celery",
+		"celery",
+		"celery",
+		"celery",
+		"crow",
+		"crow",
+		"crow",
+		"crow",
+		"crow",
+		"crow",
+		"crow",
+		"crow",
+		"crow",
+		"crow",
+		"crow"
+		];
+	this.limboPile = [];
+	this.discardPile = [];
+	this.shuffle = function(){
+		this.drawPile = this.drawPile.concat(this.discardPile);
+		this.discardPile = [];
+		shuffle(this.drawPile);
+	}
+	this.draw3 = function(){
+		for(var i=0; i<3; i++){
+			this.limboPile.push(this.drawPile.shift());
+		}
+	}
+}
+
+function Game(){
+	this.players = [];
+	this.pres_idx = null;
+	this.chanc_idx = null;
+	this.inPower = false;
+	this.deck = new Deck();
+	this.progress = {
+		celeries: 0,
+		crows: 0
+	}
+	this.addPlayer = function(id, name){
+		this.players.push(new Player(id, name));
+	}
+	this.assignRoles = function(){
+		var roles = ["cel", "cel", "cel", "crow", "master"];
+		shuffle(roles);
+		for(var i=0; i<this.players.length; i++){
+			this.players[i].role = roles[i];
+		}
+	}
+	this.startGame = function() {
+		if(players.length != 5) {
+			//Error?
+		}
+		this.deck.shuffle();
+		this.assignRoles();
+		this.pres_idx = Math.floor(math.random()*5);
+	}
+	this.chooseChanc = function(idx) {
+		this.chanc_idx = idx;
+	}
+	this.tallyVotes = function() {
+		var yes = 0;
+		for(var i=0; i<players.length; i++){
+			if(players[i].voteStatus){
+				yes += 1;
+			}
+		}
+		if(yes>2){
+			this.inPower = true;
+			return true;
+		}
+		return false;
+	}
+	this.draw = function() {
+		this.deck.draw3();
+		return this.deck.limboPile;
+	}
+	this.discard = function(discardCard) {
+		this.discardPile.push(discardCard);
+		for(var i=0;i<this.deck.limboPile.length;i++){
+			if(this.deck.limboPile[i]==discardCard){
+				this.deck.limboPile.splice(i, 1);
+			}
+		}
+	}
+}
